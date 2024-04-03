3c2a857b3fd11188aa541856d4f9164770116e7f
Georgi <gurumov93@gmail.com>
2024-04-03 13:43:51 +0100

    About page

--- src/app/modules/core/core-routing.module.ts
+++ src/app/modules/core/core-routing.module.ts
@@ -1,8 +1,10 @@
 import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
-const routes = [{ path: 'home', component: HomeComponent }];
+const routes = [{ path: 'home', component: HomeComponent }, { path: 'about', component: AboutPageComponent }];
+
 
 @NgModule({
   declarations: [],
--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -5,9 +5,10 @@ import { CoreRoutingModule } from './core-routing.module';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { SharedModule } from '../shared/shared.module';
 import { FireModule } from '../fire/fire.module';
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
 @NgModule({
-  declarations: [HomeComponent, NavigationComponent],
+  declarations: [HomeComponent, NavigationComponent, AboutPageComponent],
   imports: [CommonModule, CoreRoutingModule, SharedModule, FireModule],
   exports: [NavigationComponent],
 })
